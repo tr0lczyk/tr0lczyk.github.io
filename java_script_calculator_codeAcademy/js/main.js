65627599f48976d287c2aa41bd8545a756e0b548
tr0lczyk <matt.olczyk@gmail.com>
2017-10-27 23:08:19 +0200

    zmiany

--- java_script_calculator_codeAcademy/js/main.js
+++ java_script_calculator_codeAcademy/js/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
+    var testNumLength = function(number) {
+        if (number.length > 9) {
+            totaldiv.text(number.substr(number.length-9,9));
+            if (number.length > 15) {
+                number = "";
+                totaldiv.text("Err");
+            }
+        } 
+    };
+	var number = "";
+	var newnumber = "";
+	var operator = "";
+	var totaldiv = $('#total');
+    totaldiv.text("0");
+    
+    $('numbers > a').not('#clear','#clearall').click(function(){
+        number += $(this).text();
+        totaldix.text(number);
+        testNumLength(number);
+    })
     
 });
