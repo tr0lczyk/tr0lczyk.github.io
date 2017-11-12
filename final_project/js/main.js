dd7156225542d834396394f535d9555028ee2108
tr0lczyk <matt.olczyk@gmail.com>
2017-11-12 16:45:47 +0100

    updates

--- final_project/js/main.js
+++ final_project/js/main.js
@@ -1,10 +1,54 @@
-$(document).ready(function(){
-    console.log('ready!');
-    $.getJSON( "https://blockchain.info/pl/ticker", function( data ){
-        console.log(data);
-        console.log(data.PLN.buy , data.PLN.sell);
-        $('#buy').html(data.PLN.buy);
-        $('#sell').html(data.PLN.sell);
+$(function(){
+    
+    
+    var interval = null;
+    
+    function getDataExchange() {
+        
+        var buyArrow = $('#buy-arrow')
+        var currentBuyPrice = parseFloat($('#buy').html());
+        
+        var sellArrow = $('#sell-arrow')
+        var currentSellPrice = parseFloat($('#sell').html());
+        
+        $.getJSON( "https://blockchain.info/pl/ticker", function( data ){
+            $('#buy').html(data.PLN.buy);
+            $('#sell').html(data.PLN.sell);
+            
+            if(currentBuyPrice < parseFloat(data.PLN.buy)){
+                buyArrow.css('color', 'green').removeClass().addClass('fa fa-arrow-up');
+                console.log('increase');
+            } else if(currentBuyPrice > parseFloat(data.PLN.buy)){
+                buyArrow.css('color', 'red').removeClass().addClass('fa fa-arrow-down');
+                console.log('decrease');
+            } else {
+                buyArrow.css('color', 'black').removeClass().addClass('fa fa-minus-square-o');
+                console.log('no change');
+            };
+            
+            if(currentSellPrice < parseFloat(data.PLN.sell)){
+                buyArrow.css('color', 'green').removeClass().addClass('fa fa-arrow-up');
+                console.log('increase');
+            } else if(currentSellPrice > parseFloat(data.PLN.sell)){
+                buyArrow.css('color', 'red').removeClass().addClass('fa fa-arrow-down');
+                console.log('decrease');
+            } else {
+                buyArrow.css('color', 'black').removeClass().addClass('fa fa-minus-square-o');
+                console.log('no change');
+            };
+            
+            
+        });
+    }
+    
+    getDataExchange();
+    
+    interval = setInterval(getDataExchange, 5000);
+    
+    $('.control-button').click('on', function(){
+        clearInterval(interval);
+        interval = setInterval(getDataExchange, $(this).val());
+        $('#refresh-frequency').html($(this).text());
     });
     
 });  
