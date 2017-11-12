261dbda3d19d71705566087092ccf2a16a4d9e40
tr0lczyk <matt.olczyk@gmail.com>
2017-11-12 15:42:20 +0100

    adding buy/sell

--- final_project/js/main.js
+++ final_project/js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
+    console.log('ready!');
+    $.getJSON( "https://blockchain.info/pl/ticker", function( data ){
+        console.log(data);
+        console.log(data.PLN.buy , data.PLN.sell);
+        $('#buy').html(data.PLN.buy);
+        $('#sell').html(data.PLN.sell);
+    });
     
-})
+});  
+
